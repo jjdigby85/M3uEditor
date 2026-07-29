789bfddfb8cb86957d2aa6728b572c48124a6d80
jjdigby85 <279348937+jjdigby85@users.noreply.github.com>
2026-07-28 23:22:50 -0400

    Update GlobalSearch.vue

--- frontend/src/views/GlobalSearch.vue
+++ frontend/src/views/GlobalSearch.vue
@@ -47,7 +47,7 @@
 
 <script setup>
 import { ref } from 'vue'
-import api from '@/api' // Axios instance with auth headers
+import api from '../api' // Axios instance with auth headers
 
 const searchQuery = ref('')
 const selectedType = ref('')
